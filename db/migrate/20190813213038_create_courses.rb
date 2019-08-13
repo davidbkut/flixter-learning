4f0dfbe3d180ea50a747151cf047db1912ff3f1c
davidbkut <davidbk1974@gmail.com>
2019-08-13 23:08:20 +0000

    Flixter 13 Page Not Found

--- db/migrate/20190813213038_create_courses.rb
+++ db/migrate/20190813213038_create_courses.rb
@@ -1,13 +1,12 @@
 class CreateCourses < ActiveRecord::Migration[5.2]
-  def change
+def change
     create_table :courses do |t|
-            t.string :title
+      t.string :title
       t.text :description
       t.decimal :cost
       t.integer :user_id
       t.timestamps
-      t.timestamps
     end
-      add_index :course, :user_id
+    add_index :courses, :user_id
   end
 end
