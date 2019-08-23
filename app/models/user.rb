3e4715bbe6141f411d55a59aba42c45e4977e744
davidbkut <davidbk1974@gmail.com>
2019-08-23 01:52:27 +0000

    Flixter 23 Error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_many :courses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
